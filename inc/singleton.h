5ce638d394bcc9a1f73566dcf19549a109ec134b
zachary <zachary_k@163.com>
2022-12-10 00:15:00 +0800

    update singleton

--- inc/singleton.h
+++ inc/singleton.h
@@ -1,1 +1,31 @@
 #pragma once
+
+namespace ulities{
+
+template <typename T>
+class singleton
+{
+private:
+    singleton(/* args */){}
+    ~singleton(){}
+    singleton(const singleton<T> &){}
+    singleton<T> & operator = (const singleton<T> &);
+
+private:
+    static T * m_instance;
+
+public:
+    static T * Get_Instance(){
+        if (m_instance == NULL)
+        {
+            m_instance = new T();
+        }
+        return m_instance;
+    }
+    
+};
+
+template <typename T>
+T * singleton<T>::m_instance = NULL;
+
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+#include <iostream>
+#include "inc/A.h"
 #include "inc/singleton.h"
 
 int main(int argc, char const *argv[])
 {
+
+    singleton<A>::Get_Instance()->show();
+    
     /* code */
     return 0;
 }
