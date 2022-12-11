9721e5afb5fc14cec609cd2cea4c5fca2f608d49
zachary <zachary_k@163.com>
2022-12-11 17:26:17 +0800

    update operator overload

---
+++ for_test/operator_overload_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class A 
+{
+private:
+    string a;
+public:
+    A(string n):a(n){}
+    ~ A(){}
+    string get(void){return a;}
+    A operator+(A& b){
+        A result = a + b.a;
+        return result;
+    }
+};
+ 
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    A a = string("this is a string param\n");
+    A b = string("this is b string param\n");
+    A c = a + b;
+    cout << c.get() << endl; 
+    return 0;
+}
+
+
+
