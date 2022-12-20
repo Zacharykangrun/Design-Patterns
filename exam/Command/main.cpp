7caf309f2604958924d9617bd00129bfc735173c
zachary <zachary_k@163.com>
2022-12-20 22:50:00 +0800

    update case

--- exam/Command/main.cpp
+++ exam/Command/main.cpp
@@ -8,9 +8,9 @@ int main(int argc, char const *argv[])
     Client Arxbibye;
     Waitress waitress; 
     vector<string> order;
-    order.push_back("波士顿大龙虾");
-    order.push_back("刀削面");
-    order.push_back("猫屎咖啡");
+    order.push_back("lobster");
+    order.push_back("noodles");
+    order.push_back("coffee");
     Arxbibye.orderFood(waitress, order);
 
     return 0;
